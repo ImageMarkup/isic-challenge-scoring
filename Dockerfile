ac85c2ad669a80e6d1946bc837c359cb4fdbaf54
Brian Helba <brian.helba@kitware.com>
2016-03-24 10:34:59 -0400

    Add scoring for Part 1

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu:14.04
+MAINTAINER Brian Helba <brian.helba@kitware.com>
+
+RUN mkdir /covalic
+
+COPY Python /covalic/Python
+COPY Data /covalic/Data
+
+# Install system prerequisites
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    freeglut3-dev \
+    git \
+    mesa-common-dev \
+    python \
+    python-pip \
+    libpython-dev \
+    liblapack-dev \
+    gfortran
+
+RUN pip install numpy scipy
+
+ENTRYPOINT ["python", "/covalic/Python/scoreSubmission.py"]
---
+++ Python/scoreSubmission.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import json
+import os
+import re
+import sys
+import zipfile
+
+from PIL import Image
+import numpy as np
+
+
+def extractZip(path, dest, flatten=True):
+    """
+    Extract a zip file, optionally flattening it into a single directory.
+    """
+    try:
+        os.makedirs(dest)
+    except OSError:
+        if not os.path.exists(dest):
+            raise
+
+    with zipfile.ZipFile(path) as zf:
+        if flatten:
+            for name in zf.namelist():
+                out = os.path.join(dest, os.path.basename(name))
+                with open(out, 'wb') as ofh:
+                    with zf.open(name) as ifh:
+                        while True:
+                            buf = ifh.read(65536)
+                            if buf:
+                                ofh.write(buf)
+                            else:
+                                break
+        else:
+            zf.extractall(output)
+
+
+def matchInputFile(truthFile, testDir):
+    # truthFile ~= 'ISIC_0000003_Segmentation.png'
+    for testFile in os.listdir(testDir):
+        truthFileId = truthFile.split('_')[1]
+        if truthFileId in testFile:
+            testPath = os.path.join(testDir, testFile)
+            return testPath
+
+    raise Exception('No matching submission image for: %s' % truthFile)
+
+
+def loadImage(imagePath, rsize=-1):
+    try:
+        image = Image.open(imagePath)
+    except:
+        raise Exception('Could not decode image: %s' % imagePath)
+
+    if image.mode != 'L':
+        raise Exception('Image %s is not single-channel (grayscale).' %
+                        os.path.basename(testPath))
+
+    return np.array(image)
+
+
+def runScoringP1(truthPath, testPath):
+    truthImage = loadImage(truthPath)
+    testImage = loadImage(testPath)
+
+    if testImage.shape[0:2] != truthImage.shape[0:2]:
+        raise Exception('Image %s has dimensions %s; expected %s.' %
+                        (os.path.basename(testPath), testImage.shape[0:2],
+                         truthImage.shape[0:2]))
+
+    truthBinaryImage = (truthImage > 128)
+    testBinaryImage = (testImage > 128)  # TODO: make dynamic
+
+    truthBinaryNegativeImage = 1 - truthBinaryImage
+    testBinaryNegativeImage = 1 - testBinaryImage
+
+    truthPixelSum = float(np.sum(truthBinaryImage))
+    testPixelSum = float(np.sum(testBinaryImage))
+
+    truePositive = float(np.sum(np.logical_and(truthBinaryImage,
+                                               testBinaryImage)))
+    trueNegative = float(np.sum(np.logical_and(truthBinaryNegativeImage,
+                                               testBinaryNegativeImage)))
+    falsePositive = float(np.sum(np.logical_and(truthBinaryNegativeImage,
+                                                testBinaryImage)))
+    falseNegative = float(np.sum(np.logical_and(truthBinaryImage,
+                                                testBinaryNegativeImage)))
+
+    metrics = [
+        {
+            'name': 'accuracy',
+            'value': (truePositive + trueNegative) /
+                     (truePositive + trueNegative + falsePositive + falseNegative)
+        },
+        {
+            'name': 'jaccard',
+            'value': (truePositive) / (truePositive + falseNegative + falsePositive)
+        },
+        {
+            'name': 'dice',
+            'value': (2 * truePositive) / (truthPixelSum + testPixelSum)
+        },
+        {
+            'name': 'sensitivity',
+            'value': (truePositive) / (truePositive + falseNegative)
+        },
+        {
+            'name': 'specificity',
+            'value': (trueNegative) / (trueNegative + falsePositive)
+        }
+    ]
+    return metrics
+
+
+def scoreAll(args):
+    truthDir = '/covalic/Data/ISBI2016_ISIC_Part1_Test_GroundTruth'
+
+    # Unzip the input files into appropriate folders
+    testZipPath = args.submission
+    testBaseDir = os.path.dirname(testZipPath)
+    testDir = os.path.join(testBaseDir, 'submission')
+    extractZip(testZipPath, testDir)
+
+    # Iterate over each file and call scoring executable on the pair
+    scores = []
+    for truthFile in os.listdir(truthDir):
+        # truthFile ~= 'ISIC_0000003_Segmentation.png'
+        try:
+            testPath = matchInputFile(truthFile, testDir)
+            truthPath = os.path.join(truthDir, truthFile)
+
+            datasetName = truthFile.rsplit('_', 1)[0]
+            metrics = runScoringP1(truthPath, testPath)
+        except Exception as e:
+            print(str(e), file=sys.stderr)
+            # TODO: Don't fail completely
+            raise
+
+        scores.append({
+            'dataset': datasetName,
+            'metrics': metrics
+        })
+
+    print(json.dumps(scores))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Submission scoring helper script')
+    # parser.add_argument('-g', '--groundtruth', required=True,
+    #                     help='path to the ground truth zip file')
+    parser.add_argument('-s', '--submission', required=True,
+                        help='path to the submission zip file')
+    args = parser.parse_args()
+
+    scoreAll(args)
