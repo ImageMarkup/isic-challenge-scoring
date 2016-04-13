1432d1b29147cdf99fee9b965d39dcdddd9afd88
Max Smolens <max.smolens@kitware.com>
2016-04-13 11:54:56 -0400

    Revise Phase 2B scoring
    Score all images for each feature as a single collection.

--- Python/scoreP2B.py
+++ Python/scoreP2B.py
@@ -2,10 +2,13 @@
 
 import os
 
-
+import numpy as np
+from PIL import Image
+from collections import defaultdict
 
 from scoreCommon import ScoreException, matchInputFile, loadSegmentationImage, \
-    computeCommonMetrics, computeSimilarityMetrics
+    resizeImage, convertToNumPyArray, computeCommonMetrics, \
+    computeSimilarityMetrics
 
 
 def matchFeatureInputFile(truthFile, testDir):
@@ -26,16 +29,7 @@ def matchFeatureInputFile(truthFile, testDir):
     return testPathCandidates[0]
 
 
-
-def scoreP2BImage(truthPath, testPath):
-    truthImage = loadSegmentationImage(truthPath)
-    testImage = loadSegmentationImage(testPath)
-
-    if testImage.shape[0:2] != truthImage.shape[0:2]:
-        raise ScoreException('Image %s has dimensions %s; expected %s.' %
-                             (os.path.basename(testPath), testImage.shape[0:2],
-                              truthImage.shape[0:2]))
-
+def scoreP2BConcatenatedImages(truthImage, testImage):
     truthBinaryImage = (truthImage > 128)
     testBinaryImage = (testImage > 128)
 
@@ -45,18 +39,57 @@ def scoreP2BImage(truthPath, testPath):
 
 
 def scoreP2B(truthDir, testDir):
-    # Iterate over each file and call scoring executable on the pair
     scores = []
+    features = defaultdict(list)
+
+    # Verify that test filenames match truth filenames
     for truthFile in sorted(os.listdir(truthDir)):
         testPath = matchFeatureInputFile(truthFile, testDir)
         truthPath = os.path.join(truthDir, truthFile)
 
         # truthFile ~= 'ISIC_0000003_globules.png'
         datasetName = os.path.splitext(truthFile)[0]
-        metrics = scoreP2BImage(truthPath, testPath)
+
+        featureName = datasetName.split('_')[2]
+        features[featureName].append((truthPath, testPath))
+
+    # Dimensions of resized images to add to concatenated arrays
+    width = 512
+    height = 512
+
+    for featureName, paths in features.viewitems():
+        # Pre-allocate concatenated arrays
+        numFiles = len(paths)
+        truthArray = np.zeros(numFiles * width * height, np.uint8)
+        testArray = np.zeros(numFiles * width * height, np.uint8)
+
+        for index, (truthPath, testPath) in enumerate(paths):
+            # Load images and verify that dimensions match
+            truthImage = loadSegmentationImage(truthPath)
+            testImage = loadSegmentationImage(testPath)
+
+            if testImage.size != truthImage.size:
+                raise ScoreException('Image %s has dimensions %s; expected %s.' %
+                                     (os.path.basename(testPath), testImage.size,
+                                      truthImage.size))
+
+            # Resize images
+            truthImage = resizeImage(truthImage, width, height)
+            testImage = resizeImage(testImage, width, height)
+
+            # Convert images
+            truthImage = convertToNumPyArray(truthImage)
+            testImage = convertToNumPyArray(testImage)
+
+            # Add images to concatenated arrays
+            truthArray[index * width * height : (index+1) * width * height] = truthImage.flatten()
+            testArray[index * width * height : (index+1) * width * height] = testImage.flatten()
+
+        # Score concatenated images
+        metrics = scoreP2BConcatenatedImages(truthArray, testArray)
 
         scores.append({
-            'dataset': datasetName,
+            'dataset': 'collection_' + featureName,
             'metrics': metrics
         })
 
