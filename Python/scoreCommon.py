ImageMarkup/isic-challenge-scoring

commit 1432d1b29147cdf99fee9b965d39dcdddd9afd88

Revise Phase 2B scoring
Score all images for each feature as a single collection.

Python/scoreCommon.py
@@ -45,6 +45,16 @@ def loadSegmentationImage(imagePath):
         raise ScoreException('Image %s is not single-channel (grayscale).' %
                              os.path.basename(imagePath))
 
+    return image
+
+
+def resizeImage(image, width, height):
+    """Resize image to (width, height) using nearest neighbor interpolation."""
+    return image.resize((width, height), Image.NEAREST)
+
+
+def convertToNumPyArray(image):
+    """Convert PIL Image to NumPy array."""
     image = np.array(image)
 
     imageValues = set(np.unique(image))
@@ -63,7 +73,6 @@ def loadSegmentationImage(imagePath):
         raise ScoreException('Image %s contains values other than 0 and 255.' %
                              os.path.basename(imagePath))
 
-    # TODO: resize image?
     return image
 
 

Python/scoreP1.py
@@ -5,12 +5,15 @@ import os
 
 
 from scoreCommon import ScoreException, matchInputFile, loadSegmentationImage, \
-    computeCommonMetrics, computeSimilarityMetrics
+    convertToNumPyArray, computeCommonMetrics, computeSimilarityMetrics
 
 
 def scoreP1Image(truthPath, testPath):
     truthImage = loadSegmentationImage(truthPath)
     testImage = loadSegmentationImage(testPath)
+
+    truthImage = convertToNumPyArray(truthImage)
+    testImage = convertToNumPyArray(testImage)
 
     if testImage.shape[0:2] != truthImage.shape[0:2]:
         raise ScoreException('Image %s has dimensions %s; expected %s.' %

Python/scoreP2B.py
@@ -2,10 +2,13 @@
 
 import os
 
-
+import numpy as np
+from PIL import Image
+from collections import defaultdict
 
 from scoreCommon import ScoreException, matchInputFile, loadSegmentationImage, \
-    computeCommonMetrics, computeSimilarityMetrics
+    resizeImage, convertToNumPyArray, computeCommonMetrics, \
+    computeSimilarityMetrics
 
 
 def matchFeatureInputFile(truthFile, testDir):
@@ -26,16 +29,7 @@ def matchFeatureInputFile(truthFile, testDir):
     return testPathCandidates[0]
 
 
-
-def scoreP2BImage(truthPath, testPath):
-    truthImage = loadSegmentationImage(truthPath)
-    testImage = loadSegmentationImage(testPath)
-
-    if testImage.shape[0:2] != truthImage.shape[0:2]:
-        raise ScoreException('Image %s has dimensions %s; expected %s.' %
-                             (os.path.basename(testPath), testImage.shape[0:2],
-                              truthImage.shape[0:2]))
-
+def scoreP2BConcatenatedImages(truthImage, testImage):
     truthBinaryImage = (truthImage > 128)
     testBinaryImage = (testImage > 128)
 
@@ -45,18 +39,57 @@ def scoreP2BImage(truthPath, testPath):
 
 
 def scoreP2B(truthDir, testDir):
-    # Iterate over each file and call scoring executable on the pair
     scores = []
+    features = defaultdict(list)
+
+    # Verify that test filenames match truth filenames
     for truthFile in sorted(os.listdir(truthDir)):
         testPath = matchFeatureInputFile(truthFile, testDir)
         truthPath = os.path.join(truthDir, truthFile)
 
         # truthFile ~= 'ISIC_0000003_globules.png'
         datasetName = os.path.splitext(truthFile)[0]
-        metrics = scoreP2BImage(truthPath, testPath)
+
+        featureName = datasetName.split('_')[2]
+        features[featureName].append((truthPath, testPath))
+
+    # Dimensions of resized images to add to concatenated arrays
+    width = 512
+    height = 512
+
+    for featureName, paths in features.viewitems():
+        # Pre-allocate concatenated arrays
+        numFiles = len(paths)
+        truthArray = np.zeros(numFiles * width * height, np.uint8)
+        testArray = np.zeros(numFiles * width * height, np.uint8)
+
+        for index, (truthPath, testPath) in enumerate(paths):
+            # Load images and verify that dimensions match
+            truthImage = loadSegmentationImage(truthPath)
+            testImage = loadSegmentationImage(testPath)
+
+            if testImage.size != truthImage.size:
+                raise ScoreException('Image %s has dimensions %s; expected %s.' %
+                                     (os.path.basename(testPath), testImage.size,
+                                      truthImage.size))
+
+            # Resize images
+            truthImage = resizeImage(truthImage, width, height)
+            testImage = resizeImage(testImage, width, height)
+
+            # Convert images
+            truthImage = convertToNumPyArray(truthImage)
+            testImage = convertToNumPyArray(testImage)
+
+            # Add images to concatenated arrays
+            truthArray[index * width * height : (index+1) * width * height] = truthImage.flatten()
+            testArray[index * width * height : (index+1) * width * height] = testImage.flatten()
+
+        # Score concatenated images
+        metrics = scoreP2BConcatenatedImages(truthArray, testArray)
 
         scores.append({
-            'dataset': datasetName,
+            'dataset': 'collection_' + featureName,
             'metrics': metrics
         })
 

commit 7c538b19bbfcb3e0c58e886829d85899d305c56c

Fix usage of undefined variable name
This fixes a regression introduced in 1432d1b.

The convertToNumPyArray() function uses the 'imagePath' variable when raising a
ScoreException, but after splitting the original function that variable no
longer exists.

Python/scoreCommon.py
@@ -55,7 +55,7 @@ def resizeImage(image, width, height):
     return image.resize((width, height), Image.NEAREST)
 
 
-def convertToNumPyArray(image):
+def convertToNumPyArray(image, imagePath):
     """Convert PIL Image to NumPy array."""
     image = np.array(image)
 

Python/scoreP1.py
@@ -12,8 +12,8 @@ def scoreP1Image(truthPath, testPath):
     truthImage = loadSegmentationImage(truthPath)
     testImage = loadSegmentationImage(testPath)
 
-    truthImage = convertToNumPyArray(truthImage)
-    testImage = convertToNumPyArray(testImage)
+    truthImage = convertToNumPyArray(truthImage, truthPath)
+    testImage = convertToNumPyArray(testImage, testPath)
 
     if testImage.shape[0:2] != truthImage.shape[0:2]:
         raise ScoreException('Image %s has dimensions %s; expected %s.' %

Python/scoreP2B.py
@@ -78,8 +78,8 @@ def scoreP2B(truthDir, testDir):
             testImage = resizeImage(testImage, width, height)
 
             # Convert images
-            truthImage = convertToNumPyArray(truthImage)
-            testImage = convertToNumPyArray(testImage)
+            truthImage = convertToNumPyArray(truthImage, truthPath)
+            testImage = convertToNumPyArray(testImage, testPath)
 
             # Add images to concatenated arrays
             truthArray[index * width * height : (index+1) * width * height] = truthImage.flatten()