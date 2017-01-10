30d113c107ad590076a8ffbd1b8050b460ed109e
Brian Helba <brian.helba@kitware.com>
2017-01-10 12:08:34 -0500

    WIP: Update Part 3 for 2017 challenge

--- Python/scoreP3.py
+++ Python/scoreP3.py
@@ -45,9 +45,8 @@ def matchRowName(truthImageName, testValues):
     return testValueCandidates[0]
 
 
-def scoreP3(truthDir, testDir, phaseNum='3'):
-    truthFile = os.path.join(
-        truthDir, 'ISBI2016_ISIC_Part%s_Test_GroundTruth.csv' % phaseNum)
+def scoreP3(truthDir, testDir):
+    truthFile = os.path.join(truthDir, 'ISIC-2017_Test_Part3_GroundTruth.csv')
     assert os.path.exists(truthFile)
 
     testFiles = sorted(os.listdir(testDir))
@@ -60,29 +59,35 @@ def scoreP3(truthDir, testDir, phaseNum='3'):
     testRows = []
     with open(testFile, 'rU') as testFileObj:
         try:
-            testReader = csv.DictReader(testFileObj,
-                                        fieldnames=['image', 'confidence'])
+            testReader = csv.DictReader(
+                testFileObj,
+                fieldnames=['image_id', 'melanoma', 'seborrheic_keratosis'])
             for rowNum, testRow in enumerate(testReader):
                 # TODO: handle extra fields
-                if len(testRow.keys()) < 2:
+                if len(testRow.keys()) < 3:
                     raise ScoreException('Row %d has an incorrect number of'
-                                         'fields. Two fields are expected: '
-                                         '<image_id>, <malignant_confidence>' %
-                                         rowNum)
+                                         'fields. Three fields are expected: '
+                                         '<image_id>, <melanoma>, '
+                                         '<seborrheic_keratosis>' % rowNum)
 
-                if not testRow['image']:
+                if not testRow['image_id']:
                     raise ScoreException('Could not find an image ID in the '
                                          'first field of row %d.' % rowNum)
 
                 try:
-                    testRow['confidence'] = float(testRow['confidence'])
+                    testRow['melanoma'] = float(testRow['melanoma'])
+                    testRow['seborrheic_keratosis'] = \
+                        float(testRow['seborrheic_keratosis'])
                 except (ValueError, TypeError):
-                    raise ScoreException('Could not parse the second field for '
-                                         '"%s" (row %d) as a floating-point '
-                                         'value.' % (testRow['image'], rowNum))
-                if not (0.0 <= testRow['confidence'] <= 1.0):
-                    raise ScoreException('The confidence value for "%s" (row %d)'
-                                         ' is outside the range [0.0, 1.0].' %
+                    raise ScoreException('Could not parse one of the second or '
+                                         'third fields for "%s" (row %d) as '
+                                         'floating-point values.' %
+                                         (testRow['image'], rowNum))
+                if not ((0.0 <= testRow['melanoma'] <= 1.0) and
+                        (0.0 <= testRow['seborrheic_keratosis'] <= 1.0)):
+                    raise ScoreException('One of the confidence values for '
+                                         '"%s" (row %d) is outside the range '
+                                         '[0.0, 1.0].' %
                                          (testRow['image'], rowNum))
 
                 testRows.append(testRow)
@@ -93,21 +98,22 @@ def scoreP3(truthDir, testDir, phaseNum='3'):
     # Load the ground truth CSV file, and merge it with the test results
     combinedRows = []
     with open(truthFile) as truthFileObj:
-        truthReader = csv.DictReader(truthFileObj,
-                                     fieldnames=['image', 'confidence'])
+        truthReader = csv.DictReader(truthFileObj)
         for truthRow in truthReader:
             # Find the matching test result
-            testRow = matchRowName(truthRow['image'], testRows)
+            testRow = matchRowName(truthRow['image_id'], testRows)
 
             combinedRows.append({
-                'image': truthRow['image'],
-                'truth_value': float(truthRow['confidence']),
-                'test_value': testRow['confidence'],
+                'image_id': truthRow['image_id'],
+                'truth_value_mel': float(truthRow['melanoma']),
+                'test_value_mel': testRow['melanoma'],
+                'truth_value_sebk': float(truthRow['seborrheic_keratosis']),
+                'test_value_sebk': testRow['seborrheic_keratosis'],
             })
 
     # Build the Numpy arrays for calculations
-    truthValues = np.array([value['truth_value'] for value in combinedRows])
-    testValues = np.array([value['test_value'] for value in combinedRows])
+    truthValues = np.array([value['truth_value_mel'] for value in combinedRows])
+    testValues = np.array([value['test_value_mel'] for value in combinedRows])
 
     # Compute accuracy, sensitivity, and specificity
     truthBinaryValues = truthValues > 0.5
@@ -116,16 +122,16 @@ def scoreP3(truthDir, testDir, phaseNum='3'):
 
     # Compute average precision
     metrics.extend(computeAveragePrecisionMetrics(truthValues, testValues))
-    
+
     # Compute AUC
     metrics.extend(computeAUCMetrics(truthValues, testValues))
-    
+
     # Compute specificity at 95% sensitivity
     metrics.extend(computeSPECMetrics(truthValues, testValues, 0.95))
-    
+
     # Compute specificity at 98% sensitivity
     metrics.extend(computeSPECMetrics(truthValues, testValues, 0.98))
-    
+
     # Compute specificity at 99% sensitivity
     metrics.extend(computeSPECMetrics(truthValues, testValues, 0.99))
 
