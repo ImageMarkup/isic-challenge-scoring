f7652d91ab32601ceeb65b6b0123380935102a7c
Brian Helba <brian.helba@kitware.com>
2019-03-04 19:48:32 -0500

    Fix style errors

--- test/test_task1.py
+++ test/test_task1.py
@@ -57,21 +57,21 @@ oneExtraPredictionBinaryImage = np.array([
     'truthBinaryImage,predictionBinaryImage,'
     'thresholdJaccard,jaccard,dice,sensitivity,specificity,accuracy', [
         (truthBinaryImage, emptyOverlapPredictionBinaryImage,
-         0.0, 0/4, 2*0/4, 0/4, 12/12, 12/16),
+         0.0, 0/4, 2*0/4, 0/4, 12/12, 12/16),  # noqa: E226
         (truthBinaryImage, noOverlapPredictionBinaryImage,
-         0.0, 0/8, 2*0/8, 0/4, 8/12, 8/16),
+         0.0, 0/8, 2*0/8, 0/4, 8/12, 8/16),  # noqa: E226
         (truthBinaryImage, quarterOverlapPredictionBinaryImage,
-         0.0, 1/7, 2*1/8, 1/4, 9/12, 10/16),
+         0.0, 1/7, 2*1/8, 1/4, 9/12, 10/16),  # noqa: E226
         (truthBinaryImage, halfOverlapPredictionBinaryImage,
-         0.0, 2/6, 2*2/8, 2/4, 10/12, 12/16),
+         0.0, 2/6, 2*2/8, 2/4, 10/12, 12/16),  # noqa: E226
         (truthBinaryImage, halfFilledPredictionBinaryImage,
-         0.0, 2/4, 2*2/6, 2/4, 12/12, 14/16),
+         0.0, 2/4, 2*2/6, 2/4, 12/12, 14/16),  # noqa: E226
         (truthBinaryImage, threeQuarterFilledPredictionBinaryImage,
-         3/4, 3/4, 2*3/7, 3/4, 12/12, 15/16),
+         3/4, 3/4, 2*3/7, 3/4, 12/12, 15/16),  # noqa: E226
         (truthBinaryImage, truthBinaryImage,
-         4/4, 4/4, 2*4/8, 4/4, 12/12, 16/16),
+         4/4, 4/4, 2*4/8, 4/4, 12/12, 16/16),  # noqa: E226
         (truthBinaryImage, oneExtraPredictionBinaryImage,
-         4/5, 4/5, 2*4/9, 4/4, 11/12, 15/16),
+         4/5, 4/5, 2*4/9, 4/4, 11/12, 15/16),  # noqa: E226
     ]
 )
 def test_scoreImage(
