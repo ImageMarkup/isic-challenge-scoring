8251df835277eef89477145606ea0a77ecce4df0
Brian Helba <brian.helba@kitware.com>
2019-04-09 16:29:35 -0400

    Continuous release via CI

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+import os
+
 from setuptools import find_packages, setup
+
+
+def prerelease_local_scheme(version):
+    """
+    Return local scheme version unless building on master in CircleCI.
+
+    This function returns the local scheme version number
+    (e.g. 0.0.0.dev<N>+g<HASH>) unless building on CircleCI for a
+    pre-release in which case it ignores the hash and produces a
+    PEP440 compliant pre-release version number (e.g. 0.0.0.dev<N>).
+    """
+    from setuptools_scm.version import get_local_node_and_date
+
+    if os.getenv('CIRCLE_BRANCH') == 'master':
+        return ''
+    else:
+        return get_local_node_and_date(version)
+
 
 setup(
     name='isic_challenge_scoring',
@@ -9,4 +29,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     python_requires='>=3.7.0',
     install_requires=['click', 'numpy', 'pandas', 'pillow', 'scipy', 'scikit-learn'],
+    use_scm_version={'local_scheme': prerelease_local_scheme},
+    setup_requires=['setuptools_scm'],
 )
