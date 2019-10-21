4428edcbb1fcbfef2697edc9ff07b4d81b9fb0a5
Brian Helba <brian.helba@kitware.com>
2019-10-20 21:55:21 -0400

    Use default version number for tags in CI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def prerelease_local_scheme(version) -> str:
     """
-    Return local scheme version unless building on master in CircleCI.
+    Return local scheme version unless building a tag or master in CircleCI.
 
     This function returns the local scheme version number
     (e.g. 0.0.0.dev<N>+g<HASH>) unless building on CircleCI for a
@@ -15,8 +15,9 @@ def prerelease_local_scheme(version) -> str:
     """
     from setuptools_scm.version import get_local_node_and_date
 
+    circleci_tag = os.getenv('CIRCLE_TAG')
     circleci_branch = os.getenv('CIRCLE_BRANCH')
-    if circleci_branch == 'master':
+    if circleci_tag or (circleci_branch == 'master'):
         return ''
     else:
         return get_local_node_and_date(version)
