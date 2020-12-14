38c0c4d5a600065e7d2a9b50cef8cb4d6e77e1d3
Brian Helba <brian.helba@kitware.com>
2020-12-14 10:38:39 -0500

    Switch to GitHub Actions for CI and automated release publication

--- isic_challenge_scoring/__init__.py
+++ isic_challenge_scoring/__init__.py
@@ -1,5 +1,13 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from isic_challenge_scoring.classification import ClassificationScore, ValidationMetric
 from isic_challenge_scoring.segmentation import SegmentationScore
 from isic_challenge_scoring.types import ScoreException
 
 __all__ = ['ClassificationScore', 'SegmentationScore', 'ScoreException', 'ValidationMetric']
+
+try:
+    __version__ = version('isic-challenge-scoring')
+except PackageNotFoundError:
+    # package is not installed
+    pass
