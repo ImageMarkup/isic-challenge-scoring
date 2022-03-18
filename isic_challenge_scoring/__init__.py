6b58239b63a3608c3561ba1fafe2dd0c45a75d68
Brian Helba <brian.helba@kitware.com>
2022-03-18 11:25:22 -0400

    Add a ScoreException alias

--- isic_challenge_scoring/__init__.py
+++ isic_challenge_scoring/__init__.py
@@ -4,6 +4,8 @@ from isic_challenge_scoring.classification import ClassificationMetric, Classifi
 from isic_challenge_scoring.segmentation import SegmentationScore
 from isic_challenge_scoring.types import ScoreError
 
+ScoreException = ScoreError
+
 __all__ = ['ClassificationScore', 'SegmentationScore', 'ScoreError', 'ClassificationMetric']
 
 try:
